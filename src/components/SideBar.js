01c70858f9659fbac688fe59673a423ce6972f14
HJ Kang <hkang07@gmail.com>
2023-02-13 11:04:38 +0000

    added sort by price feature

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,39 +1,72 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import qs from "qs";
 import "../styles/sidebar.css";
 
 const SideBar = () => {
+  const buildQueryString = (operation, valueObj) => {
+    const { search } = useLocation();
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify(valueObj),
+    };
+
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
   return (
     <div className="sidebar">
       <div className="city-link-container">
         <b>Filter by city</b>
         <div className="city">
-          <Link to={`/?query={"city": "Manchester"}`} className="city-link">
+          <Link
+            to={buildQueryString("query", { city: "Manchester" })}
+            className="city-link"
+          >
             Manchester
           </Link>
         </div>
         <div className="city">
-          <Link to={`/?query={"city": "Leeds"}`} className="city-link">
+          <Link
+            to={buildQueryString("query", { city: "Leeds" })}
+            className="city-link"
+          >
             Leeds
           </Link>
         </div>
         <div className="city">
-          <Link to={`/?query={"city": "Sheffield"}`} className="city-link">
+          <Link
+            to={buildQueryString("query", { city: "Sheffield" })}
+            className="city-link"
+          >
             Sheffield
           </Link>
         </div>
         <div className="city">
-          <Link to={`/?query={"city": "Liverpool"}`} className="city-link">
+          <Link
+            to={buildQueryString("query", { city: "Liverpool" })}
+            className="city-link"
+          >
             Liverpool
           </Link>
         </div>
         <div className="city">
-          <Link to={`/?query={"city": "Birmingham"}`} className="city-link">
+          <Link
+            to={buildQueryString("query", { city: "Birmingham" })}
+            className="city-link"
+          >
             Birmingham
           </Link>
         </div>
         <div className="city">
-          <Link to={`/?query={"city": "London"}`} className="city-link">
+          <Link
+            to={buildQueryString("query", { city: "London" })}
+            className="city-link"
+          >
             London
           </Link>
         </div>
@@ -41,12 +74,18 @@ const SideBar = () => {
       <div className="sort-container">
         <b>Sort by</b>
         <div className="price">
-          <Link to="/" className="price-link">
+          <Link
+            to={buildQueryString("sort", { price: 1 })}
+            className="price-link"
+          >
             Price Ascending
           </Link>
         </div>
         <div className="price">
-          <Link to="/" className="price-link">
+          <Link
+            to={buildQueryString("sort", { price: -1 })}
+            className="price-link"
+          >
             Price Descending
           </Link>
         </div>
